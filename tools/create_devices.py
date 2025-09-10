9222be7e8c5dbb93bc7d48589ce9f1b88a4f3c50
thingscloud <github@qijiwanwu.com>
2025-09-10 12:01:23 +0800

    Update create_devices.py

--- tools/create_devices.py
+++ tools/create_devices.py
@@ -11,23 +11,29 @@ import pandas as pd
 from lib.api_common import base_url, get_access_token
 from lib.logger import log_info
 
-@click.command('create_devices')
-@click.option('--file_path', default='import/create_devices_template.csv', required=False, show_default=True, help='指定导入的 CSV 文件路径')
-@click.option('--type_id', required=True, help='为创建的设备指定设备类型 ID')
+@click.command("create_devices")
+@click.option(
+    "--file_path",
+    default="import/create_devices_template.csv",
+    required=False,
+    show_default=True,
+    help="指定导入的 CSV 文件路径",
+)
+@click.option("--type_id", required=True, help="为创建的设备指定设备类型 ID")
 
 def create_devices(file_path, type_id):
-    
+
     access_token = get_access_token()
     if access_token == None:
         click.echo("API 身份验证失败，终止程序")
         return
-    
+
     click.echo("API 身份验证成功")
     click.echo(f"开始读取导入文件 {file_path}")
-    
+
     # 读取 CSV 文件
     try:
-        df = pd.read_csv(file_path)
+        df = pd.read_csv(file_path, dtype={"name": str})
         click.echo(f"共读取 {len(df)} 个设备")
         with tqdm(total=len(df), desc="创建进度", unit="设备") as pbar:
             for index, row in df.iterrows():
@@ -44,9 +50,10 @@ def create_devices(file_path, type_id):
         print("找不到 CSV 文件")
     except Exception as e:
         print(f"打开文件发生异常: {str(e)}")
-    
+
+
 def create_device(access_token, name, device_key, type_id):
-    
+
     payload = {
         "name": name,
         "type": type_id,
@@ -54,14 +61,14 @@ def create_device(access_token, name, device_key, type_id):
     }
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {access_token}"
+        "Authorization": f"Bearer {access_token}",
     }
     api_url = base_url() + "/api/v1/device"
     try:
         response = requests.post(api_url, json=payload, headers=headers)
         response.raise_for_status()
         data = response.json()
-        if data['result']:
+        if data["result"]:
             print(f"设备 [{name}] 创建成功！返回结果: {response.json()}")
         else:
             print(f"设备 [{name}] 创建失败！返回结果: {response.json()}")
